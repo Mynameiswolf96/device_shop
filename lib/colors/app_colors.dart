f1f761168f765e385f7ac56b0f2ef3d6a9fb755e
Valery Kuchuk <kuchuk-valerochka@mail.ru>
2024-04-18 19:06:11 +0300

    add two paddings in discount and often buy catalogs

---
+++ lib/colors/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+abstract class AppColor{
+  static const Color purple=Color(0xFFAB1CAE);
+  static const Color white=Color(0xFFFFFFFF);
+  static const Color grey=Color(0xFFD9D9D9);
+}
